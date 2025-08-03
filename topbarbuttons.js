4d76d6b0a4a1635895042ef8cd308b99b794b2c0
RealMasterTonky <121755215+RealMasterTonky@users.noreply.github.com>
2025-08-03 18:59:04 +0300

    Update topbarbuttons.js

--- topbarbuttons.js
+++ topbarbuttons.js
@@ -1,8 +1,16 @@
 console.log("test")
 
-const homebutton = document.createElement("div");
-homebutton.innerText = "home"
-homebutton.onclick = function() {
+function addbuttons(topbar) {
+  const homebutton = document.createElement("button");
+  homebutton.innerText = "home"
+  homebutton.onclick = function() {
   window.location.href='https://realmastertonky.github.io/site'
+  }
+  topbar.appendChild(homebutton)
+  const towersbutton = document.createElement("button");
+  towersbutton.innerText = "my towers"
+  towersbutton.onclick = function() {
+  window.location.href='https://realmastertonky.github.io/site/towers'
+  }
+  topbar.appendChild(towersbutton)
 }
-document.getElementById("topbar").appendChild(homebutton)
