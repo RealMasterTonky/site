3474ed0fa41094664968ca3e59e0c3b5587a5738
RealMasterTonky <121755215+RealMasterTonky@users.noreply.github.com>
2025-08-03 18:43:15 +0300

    Create topbarbuttons.js

---
+++ topbarbuttons.js
@@ -0,0 +1,8 @@
+console.log("test")
+
+const homebutton = document.createElement("div");
+homebutton.innerText = "home"
+homebutton.onclick = function() {
+  window.location.href='https://realmastertonky.github.io/site'
+}
+document.body.appendChild(document.getElementsByClassName("topbar")[0])
