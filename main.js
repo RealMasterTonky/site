490ed8465f17cfb01dc402393562557c12fb82bd
RealMasterTonky <121755215+RealMasterTonky@users.noreply.github.com>
2025-10-29 15:41:18 +0000

    Update main.js

--- main.js
+++ main.js
@@ -44,7 +44,34 @@ let pages = {
             "SoBiaH": ["Steeple of Being in a Hurry", "bottom difficult", [255, 50, 50], "https://www.roblox.com/games/139955416730004/Steeple-of-Being-In-a-Hurry", false],
             "ToNK": ["Tower of No Killjoys", "mid-high difficult", [255, 50, 50], "https://www.roblox.com/games/129779678908565/Tower-of-No-Killjoys", false],
             "SoFL": ["Steeple of Feeling Lazy", "bottom challenging", [160, 0, 0], "https://www.roblox.com/games/84626206204662/Steeple-of-Feeling-Lazy", false],
+            "ToOJ": ["Tower of Orange Juice", "low-mid challenging", [160, 0, 0], "https://www.roblox.com/games/137368525411272/Tower-of-Orange-Juice", false],
+            "SoH1KVC": ["Steeple of Hardcore 1K Visits Celebration", "challenging/intense", [93, 17, 23], "https://www.roblox.com/games/94563053585293/Steeple-of-Hardcore-1K-Visits-Celebration", false],
+            "ToP!H!G.ITW!ICVMH.AIT,LP.L.R!BPI,IHTE.GN30MTTG!": ["Tower of People! Hurry! Get. In The Way! I'm Commiting Vehicular Manburger Helper! And It Tastes Like Pure. Liquid. Relief! Best Part Is, I'm Helping The Earth. Getting Negative 30 Miles To The Gallon!", "baseline intense", [25, 34, 45], "https://www.roblox.com/games/111532669293311/ToP-H-G-ITW-ICVMH-TLLR-BPI-IHTE-GN30MTTG", false],
+            "ToFaC:U": ["Tower of Fun and Creativity: Ultimate", "bottom intense", [25, 34, 45], "https://www.roblox.com/games/18858512727/Tower-of-Fun-And-Creativity-Ultimate", false],
+            "ToLR": ["Tower of Laptop Ripping", "bottom intense", [25, 34, 45], "https://www.roblox.com/games/119216713302382/Tower-of-Laptop-Ripping", false],
+            "ToAUT": ["Tower of Annoying Unoriginal Trials", "mid intense", [25, 34, 45], "https://www.roblox.com/games/135187435093971/Tower-of-Unpleasant-Surprises", false],
+            "QNS": ["Quick Neat Speedbuild", "high intense", [25, 34, 45], "https://www.roblox.com/games/112728301464577/Quick-Neat-Speedbuild", false],
+            "ToU235": ["Tower of Uranium 235", "peak intense", [25, 34, 45], "https://www.roblox.com/games/115471466040829/Tower-of-Uranium-235", true],
+            "ToMNC": ["Tower of MasterTonky Never Clear", "baseline remorseless", [200, 0, 200], "https://www.roblox.com/games/18942214044/ToMNC", false],
+            "ToDJ": ["Tower of Diagonal Jumps", "bottom-low remorseless", [200, 0, 200], "https://www.roblox.com/games/81505714555490/Tower-of-Awkward-Jumps", false],
+            "ToZ": ["Tower of Zitronda", "low remorseless", [200, 0, 200], "https://www.roblox.com/games/78940140938774/Tower-of-Zitronda", true],
+            "ToWM": ["Tower of Wedgicular Manslaughter", "high remorseless", [200, 0, 200], "https://www.roblox.com/games/89583176822766/Tower-of-Wedgicular-Manslaughter", false],
+            "SoUG": ["Steeple of Unfun GBJ", "peak remorseless", [200, 0, 200], "https://www.roblox.com/games/110278596870369/Steeple-of-Unfun-GBJ", true],
+            "SoUG": ["Tower of Eternal Darkness", "peak remorseless", [200, 0, 200], "https://www.roblox.com/games/93392183780862/Tower-of-Eternal-Darkness", false],
+            "DN:R": ["Deconfirmed Neat: Restored", "remorseless/insane", [100, 0, 228], "https://www.roblox.com/games/88005335424256/Deconfirmed-NEAT-Restored", false],
+            "OToWM": ["Old Tower of Wedgicular Manslaughter", "remorseless/insane", [100, 0, 228], "https://www.roblox.com/games/99170113213701/OToWM", true],
+            "RoUT": ["Room of Unfair Trials", "baseline insane", [0, 0, 255], "https://www.roblox.com/games/120788794283207/Room-of-Unfair-Trials", false],
+            "MT": ["Medium Tower", "bottom insane", [0, 0, 255], "https://www.roblox.com/games/80443650366413/Medium-Tower", false],
+            "ToRT": ["Tower of Random Thoughts", "mid-high insane", [0, 0, 255], "https://www.roblox.com/games/75062529132028/Tower-of-Random-Thoughts", true],
             "ToC": ["Tower of Cancer", "high-peak insane", [0, 0, 255], "https://www.roblox.com/games/18894068349/Tower-of-Cancer", true],
+            "ToIB": ["Tower of Inanimate Blockiness", "high-peak insane", [0, 0, 255], "https://www.roblox.com/games/82653933581194/Tower-of-Inanimate-Blockiness", true],
+            "ToWS": ["Tower of Wet Socks", "insane/extreme", [2, 69, 255], "https://www.roblox.com/games/74473724269500/Tower-of-Wet-Socks", true],
+            "TT": ["Triple Tower", "high extreme", [3, 137, 255], "https://www.roblox.com/games/124095927723593/Triple-Tower", true],
+            "SoMN": ["Steeple of Mewing Neat", "bottom terrifying / high end terrifying", [0, 255, 255], "https://www.roblox.com/games/18859834967/Steeple-of-Mewing-NEAT", false],
+            "ToTSP": ["Tower of Thin Stud Practice", "peak terrifying", [0, 255, 255], "https://www.roblox.com/games/76782525334704/Tower-of-Thin-Stud-Practice", true],
+            "Tfw": ["ToDIE from walmart", "totally catastrophic (low-mid difficult)", [255, 255, 255], "https://www.roblox.com/games/123467125783146/ToDIE-from-walmart", false],
+            "ToD": ["Tower of Disease", "catastrophic", [255, 255, 255], "https://www.roblox.com/games/18997549903/Tower-of-Disease", false],
+            "ToLM": ["Tower of Lunatic Masochism", "bottom-low unreal", [75, 0, 200], "https://www.roblox.com/games/133003290698864/Tower-of-Lunatic-Masochism", true],
         }
 
         for(tower in towers) {
@@ -52,9 +79,9 @@ let pages = {
             let colors = towerstats[2]
             let playable
             if(towerstats[4] === true){
-                playable = "playable!"
+                playable = "✅ playable!"
             } else {
-                playable = "unplayable."
+                playable = "❌ unplayable."
             }
             document.body.innerHTML += `<p style="color:rgb(${colors[0]},${colors[1]},${colors[2]})">${tower} - ${towerstats[0]} - ${towerstats[1]} <button onclick="window.open('${towerstats[3]}')">check (${playable})</button></p>`
         }
@@ -70,6 +97,7 @@ function loadpage() {
 document.addEventListener("DOMContentLoaded", function() {
     loadpage()
 })
+
 
 
 
