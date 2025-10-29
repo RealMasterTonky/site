a7f4446ccdb5a0162b9bb66369b14df188f55244
RealMasterTonky <121755215+RealMasterTonky@users.noreply.github.com>
2025-10-29 11:03:28 +0000

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 let topbarbuttons = {
     "home": "https://realmastertonky.github.io/site/",
+    "my artworks": "https://realmastertonky.github.io/site/artworks",
     "roblox related stuff": "https://realmastertonky.github.io/site/roblox",
 }
 
@@ -18,7 +19,12 @@ let pages = {
         document.body.innerHTML += "<h1>home</h1>"
         document.body.innerHTML += "<p>welcome to my website! for now it's about showing my things & creations</p>"
         document.body.innerHTML += "<p>so yeah, feel free to explore things here</p>"
-    }
+    },
+    "https://realmastertonky.github.io/site/roblox/": function() {
+        document.body.innerHTML += "<h1>roblox related stuff</h1>"
+        document.body.innerHTML += `<p style="font-size:34px">my games</p><br>`
+        document.body.innerHTML += "<button onclick="window.open('https://www.roblox.com/games/15954053813/Tonk-Madness')">Tonk Madness</button>"
+    },
 }
 
 function loadpage() {
@@ -30,6 +36,7 @@ function loadpage() {
 document.addEventListener("DOMContentLoaded", function() {
     loadpage()
 })
+
 
 
 
